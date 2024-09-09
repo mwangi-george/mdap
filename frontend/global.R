ea102485f6693be59646e69aa142692a406ccc49
George Mwangi <mwangigeorge648@gmail.com>
2024-09-09 08:03:15 +0300

    Updates dependencies

--- frontend/global.R
+++ frontend/global.R
@@ -16,7 +16,7 @@ options(
 )
 
 # change to false to run in dev mode
-in_production <- FALSE 
+in_production <- F
 
 # color palette for the application
 app_color_palette <- c("#5bcefa", "#00ffff", "#ffffff", "#2d677d", "#000000")
