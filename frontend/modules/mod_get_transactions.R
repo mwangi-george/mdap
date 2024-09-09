37bc22e4b4288a9eb689300c35f39f11f6c5391a
George Mwangi <mwangigeorge648@gmail.com>
2024-09-09 21:56:36 +0300

    Updated transactions module

--- frontend/modules/mod_get_transactions.R
+++ frontend/modules/mod_get_transactions.R
@@ -5,6 +5,12 @@ get_transactions_ui <- function(id){
   ns <- NS(id)
   tagList(
     fluidRow(
+      actionBttn(ns("fetch_all_transactions"), label = "Fetch Transactions"),
+      actionBttn(ns("trigger_transaction_register_button"), label = "Post Transaction"),
+      actionBttn(ns("trigger_transaction_delete_button"), label = "Delete a Transaction")
+    ),
+    br(),
+    fluidRow(
       plain_box_template(120, 4, title_text = " ", infoBoxOutput(ns("total_transactions"), width = 12)),
       plain_box_template(120, 4, title_text = " ", infoBoxOutput(ns("transaction_amount"), width = 12)),
       plain_box_template(120, 4, title_text = " ", infoBoxOutput(ns("transaction_cost"), width = 12)),
@@ -15,19 +21,7 @@ get_transactions_ui <- function(id){
       plain_box_template(
         850, 12, 
         title_text = "Transaction history",
-        actionBttn(ns("fetch_all_transactions"), label = "Fetch Transactions"),
         reactableOutput(ns("all_transactions_table"))
-      ),
-      plain_box_template(
-        250, 12, 
-        title_text  = "Post a new transaction",
-        textAreaInput(
-          inputId = ns("user_transaction_message"), 
-          label = "", 
-          placeholder = "Paste your Mpesa Message here...",
-          width = "100%", height = "150px"
-        ),
-        actionBttn(ns("register_a_transaction"), label = "Post Transaction")
       )
     )
   )
@@ -36,6 +30,8 @@ get_transactions_ui <- function(id){
 # Logic
 get_transactions_server <- function(id, auth_token, active_user){
   moduleServer(id, function(input,output, session) {
+    # namespace to access ui elements created in the server side
+    ns <- session$ns
     
     # Fetch existing transaction for current user
     transactions_4_current_user <- eventReactive(input$fetch_all_transactions, {
@@ -67,13 +63,27 @@ get_transactions_server <- function(id, auth_token, active_user){
       req(transactions_4_current_user())
       
       transactions_data_4_valuebox <- transactions_data_4_valuebox_func(transactions_4_current_user())
+      print(transactions_data_4_valuebox)
+      
+      value_to_render_func <- function(value) {
+        # custom function to format value to render in infoboxes
+        val <- suppressWarnings(
+          case_when(
+            is.null(value) ~ 0, 
+            is.character(value) ~ 0,
+            is.list(value) ~ 0,
+            .default = as.numeric(value)
+          )
+        )
+        return(val)
+      }
       
       output$total_transactions <- renderInfoBox({
         infoBox(
           width = 12,
           title = "",
           subtitle = "Total Transactions",
-          value = prettyNum(transactions_data_4_valuebox$total_transactions, big.mark = ", "),
+          value = prettyNum(value_to_render_func(transactions_data_4_valuebox$total_transactions), big.mark = ", "),
           color = "teal",
           icon = icon("laptop-code")
         )
@@ -84,7 +94,7 @@ get_transactions_server <- function(id, auth_token, active_user){
           width = 12,
           title = "",
           subtitle = "Total Amount Transacted",
-          value = prettyNum(transactions_data_4_valuebox$transaction_amount, big.mark = ", "),
+          value = prettyNum(value_to_render_func(transactions_data_4_valuebox$transaction_amount), big.mark = ", "),
           color = "teal",
           icon = icon("laptop-code")
         )
@@ -95,7 +105,7 @@ get_transactions_server <- function(id, auth_token, active_user){
           width = 12,
           title = "",
           subtitle = "Total Transaction Costs",
-          value = prettyNum(transactions_data_4_valuebox$transaction_cost, big.mark = ", "),
+          value = prettyNum(value_to_render_func(transactions_data_4_valuebox$transaction_cost), big.mark = ", "),
           color = "teal",
           icon = icon("laptop-code")
         )
@@ -129,10 +139,27 @@ get_transactions_server <- function(id, auth_token, active_user){
       
     })
     
-    # Registering a new transaction -----------
-    observeEvent(input$register_a_transaction, {
+    # Registering a transaction --------------
+    observeEvent(input$trigger_transaction_register_button, {
+      showModal(modalDialog(
+        title = "Register a transaction",
+        textAreaInput(
+          inputId = ns("user_transaction_message"), label = "", 
+          placeholder = "Paste your Mpesa Message here...",
+          width = "100%", height = "150px"
+        ),
+        actionButton(ns("perform_transaction_registration_button"), "Register", class = "btn-primary"),
+        easyClose = TRUE,
+        size = "m",
+        footer = modalButton("Close", icon = icon("arrow-right-to-bracket"))
+      ))
+    })
+    
+    
+    # Registering a new transaction
+    observeEvent(input$perform_transaction_registration_button, {
       req(input$user_transaction_message)
-      req(input$register_a_transaction)
+      req(input$perform_transaction_registration_button)
       
       post_req_endpoint <- str_c(generate_base_url(in_production), "transactions/new")
       res <- post_a_transaction(post_req_endpoint, auth_token, input$user_transaction_message)
@@ -143,9 +170,8 @@ get_transactions_server <- function(id, auth_token, active_user){
         shinyalert("Oops!", res, type = "error", closeOnClickOutside = TRUE)
       } else {
         
-        # status code
+        # status code $ api response message
         s_code <- res %>% status_code()
-        # output message
         message <- res %>% content()
         
         if (s_code == 201L) {
@@ -155,6 +181,41 @@ get_transactions_server <- function(id, auth_token, active_user){
         }
       }
     })
+    
+    # Deleting a transaction --------------
+    
+    # Trigger to transaction deletion modal
+    observeEvent(input$trigger_transaction_delete_button, {
+      showModal(modalDialog(
+        title = "Delete a transaction",
+        textAreaInput(ns("transaction_code_to_delete"), "Transaction Code", placeholder = "Enter transaction code here...", width = "100%"),
+        actionButton(ns("perform_transaction_deletion_button"), "Delete", class = "btn-primary"),
+        easyClose = TRUE,
+        size = "m",
+        footer = modalButton("Close", icon = icon("arrow-right-to-bracket"))
+      ))
+    })
+    
+    # delete the transaction
+    observeEvent(input$perform_transaction_deletion_button, {
+      req(input$transaction_code_to_delete)
+      req(input$perform_transaction_deletion_button)
+      
+      delete_req_endpoint <- str_c(generate_base_url(in_production), "transactions/delete")
+      deletion_response <- delete_a_transaction_func(delete_req_endpoint, input$transaction_code_to_delete, auth_token)
+      s_code <- deletion_response %>% status_code()
+      message <- deletion_response %>% content()
+      print(message)
+      
+      if (s_code == 200L) {
+        shinyalert("Deletion Successful!", message[[1]], type = "success", closeOnClickOutside = TRUE)
+      } else if (s_code == 404) {
+        shinyalert("Oops!", message[[1]], type = "error", closeOnClickOutside = TRUE)
+      } else if (s_code == 500L) {
+        shinyalert("Oops!", message[[1]], type = "error", closeOnClickOutside = TRUE)
+      } 
+    })
+    
   })
 }
 
