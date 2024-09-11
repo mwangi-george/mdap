9ad61de1a6371d54615da55cf4c2c7cc28f27cf3
George Mwangi <mwangigeorge648@gmail.com>
2024-09-11 23:23:12 +0300

    Changed charting library

--- frontend/modules/mod_render_transactions_charts.R
+++ frontend/modules/mod_render_transactions_charts.R
@@ -1,8 +1,8 @@
 render_transactions_chart_ui <- function(id) {
   ns <- NS(id)
   tagList(
-    plain_box_template(600, 6, title_text = " ", highchartOutput(ns("top_10_transaction_types"))),
-    plain_box_template(600, 6, title_text = " ", highchartOutput(ns("top_10_counterparties")))
+    plain_box_template(600, 6, title_text = " ", apexchartOutput(ns("top_10_transaction_types"))),
+    plain_box_template(600, 6, title_text = " ", apexchartOutput(ns("top_10_counterparties")))
   )
 }
 
@@ -10,10 +10,10 @@ render_transactions_chart_ui <- function(id) {
 render_transactions_chart_server <- function(id, transactions, active_user) {
   moduleServer(id, function(input, output, session) {
     ## charts ---------------------
-    output$top_10_transaction_types <- renderHighchart({
-      create_bar_or_column_chart_func(
+    output$top_10_transaction_types <- renderApexchart({
+      create_bar_or_column_chart_apex(
         data_with_transactions = transactions,
-        chart_type = "bar",
+        chart_type = "column",
         column_to_count = "transaction_type",
         title_text = "Most Recurring Transaction Types",
         subtitle_text = active_user,
@@ -22,15 +22,15 @@ render_transactions_chart_server <- function(id, transactions, active_user) {
       )
     })
     
-    output$top_10_counterparties <- renderHighchart({
-      create_bar_or_column_chart_func(
+    output$top_10_counterparties <- renderApexchart({
+      create_bar_or_column_chart_apex(
         data_with_transactions = transactions,
-        chart_type = "column",
+        chart_type = "bar",
         column_to_count = "counterparty",
         title_text = "Most Recurring Counterparties",
         subtitle_text = active_user,
-        x_axis_text = "Counterparty",
-        y_axis_text = "No. of transactions"
+        x_axis_text = "No. of transactions",
+        y_axis_text = "Counterparty"
       )
     })
   })
